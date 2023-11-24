f14e1d692f4997d84f68f6806e1b6517b1bf6273
Trung <dnguyen@student.42wolfsburg.de>
2023-11-24 13:59:46 +0100

    pong clone in frontend

--- ball.js
+++ ball.js
@@ -1,4 +1,5 @@
-const INITIAL_VELOCITY = 0.025
+const INITIAL_VELOCITY = 0.03
+const VELOCITY = 0.000001
 
 export default class Ball {
 	constructor(ballElem){
@@ -30,7 +31,7 @@ export default class Ball {
 	reset(){
 		this.x = 50
 		this.y = 50
-		this.direction = { x: 0.75, y: 0.5}
+		this.direction = { x: 0}
 		while (Math.abs(this.direction.x) <= 0.2 || Math.abs(this.direction.x) >= 0.9 ){
 			const heading = randomNumberBetween(0, 2 * Math.PI)
 			this.direction = { x: Math.cos(heading), y: Math.sin(heading) }
@@ -39,16 +40,17 @@ export default class Ball {
 		this.velocity = INITIAL_VELOCITY
 	}
 
-	update(delta){
+	update(delta, paddleRects){
 		this.x += this.direction.x * this.velocity * delta;
 		this.y += this.direction.y * this.velocity * delta;
+		this.velocity += VELOCITY * delta
 		const rect = this.rect()
 		if (rect.bottom >= window.innerHeight || rect.top <= 0)
 		{
 			this.direction.y *= -1
 		}
-		if (rect.right >= window.innerWidth || rect.left <= 0)
-		{
+
+		if (paddleRects.some (r => isCollission(r, rect))){
 			this.direction.x *= -1
 		}
 	}
@@ -59,3 +61,10 @@ function randomNumberBetween(min, max) {
 }
 
 
+function isCollission(rect1, rect2){
+	return(
+	rect1.left <= rect2.right && 
+	rect1.right >= rect2.left &&
+	rect1.top <= rect2.bottom &&
+	rect1.bottom >= rect2.top)
+}
--- script.js
+++ script.js
@@ -1,17 +1,83 @@
-// Update loop
 
+// main.js
 import Ball from "./ball.js";
+import Paddle from "./Paddle.js";
 
+const playerPaddle = new Paddle(document.getElementById("player-paddle"));
+const computerPaddle = new Paddle(document.getElementById("computer-paddle"));
 const ball = new Ball(document.getElementById("ball"));
+const playerScoreElem = document.getElementById("player-score");
+const computerScoreElem = document.getElementById("computer-score");
 
-let lastTime
-function update(time){
-	if (lastTime != null){
-		const delta = time -lastTime
-		ball.update(delta)
+
+const keysPressed = {}; 
+
+document.addEventListener("keydown", e => {
+	keysPressed[e.key] = true; // Set the pressed key to true in keysPressed object
+});
+
+document.addEventListener("keyup", e => {
+	delete keysPressed[e.key]; // Remove the released key from keysPressed object
+});
+
+function updatePlayerPaddlePosition() {
+	const movementStep = 2; // Adjust this value as needed for smoother movement
+
+	if (keysPressed["ArrowUp"]) {
+		const currentPosition = playerPaddle.position;
+		const newPosition = currentPosition - movementStep;
+
+		if (newPosition >= 0) {
+			playerPaddle.position = newPosition;
+		}
 	}
-	lastTime = time
-	window.requestAnimationFrame(update)
+
+	if (keysPressed["ArrowDown"]) {
+		const currentPosition = playerPaddle.position;
+		const newPosition = currentPosition + movementStep;
+
+		if (newPosition <= 100) {
+			playerPaddle.position = newPosition;
+		}
+	}
 }
 
-window.requestAnimationFrame(update)
+
+function isLose() {
+	const rect = ball.rect();
+	return rect.right >= window.innerWidth || rect.left <= 0;
+}
+
+let lastTime;
+function update(time) {
+	if (lastTime != null){
+
+		const delta = time - lastTime;
+		ball.update(delta, [playerPaddle.rect(), computerPaddle.rect()]);
+		updatePlayerPaddlePosition();
+		computerPaddle.update(delta, ball.y);
+		const hue = parseFloat(getComputedStyle(document.documentElement).getPropertyValue("--hue"));
+		document.documentElement.style.setProperty("--hue", hue + delta * 0.01)
+		if (isLose()){
+			console.log('lose')
+			handleLose();
+		}
+	}
+	lastTime = time;
+	window.requestAnimationFrame(update);
+}
+
+function handleLose(){
+	const rect = ball.rect()
+	if (rect.right >= window.innerWidth){
+		playerScoreElem.textContent = parseInt(playerScoreElem.textContent) + 1
+	}
+	else{
+		computerScoreElem.textContent = parseInt(computerScoreElem.textContent) + 1
+	}
+	ball.reset()
+	computerPaddle.reset()
+}
+
+// lastTime = performance.now();
+window.requestAnimationFrame(update);
